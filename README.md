53033d5ebcb6fd5b7dadbcfea3768b294796c9fc
GUEYE Mouhamed <mouhamedgueye945@gmail.com>
2025-03-12 16:33:47 +0100

    ajout de modèle de données

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Node-EJS
+# Node-EJS 
+
+Ce projet consiste à créer une application web dynamique affichant une liste d'événements en utilisant EJS comme moteur de rendu dans une application Node.js avec Express.
+
+## Initialisation du projet et test 
+![alt text](image.png)
+![alt text](image-1.png)
+
+## Modèle de données 
+![alt text](image-2.png)
---
+++ event.js
@@ -0,0 +1,8 @@
+const events = [
+    { id: 1, title: "Spectacle de Marionnettes", date: "2025-03-20", description: "Un show de marionnettes pour enfants.", status: "actif" },
+    { id: 2, title: "Atelier de Peinture", date: "2025-04-10", description: "Peinture créative pour enfants.", status: "actif" },
+    { id: 3, title: "Chasse au Trésor", date: "2025-05-05", description: "Une aventure à la recherche du trésor.", status: "actif" }
+  ];
+  
+  module.exports = events;
+  
